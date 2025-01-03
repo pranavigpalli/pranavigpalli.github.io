2639614619886ea56f750bb4302c7bd67ef35289
Pranavi Gollanapalli <pgollana@uci.edu>
2025-01-03 10:07:50 -0800

    completed Experiences page with information [FIXME: slight bug when resizing for smaller screens]

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,118 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div id='Experience' className='flex min-h-screen
+    w-full flex-col items-center justify-center gap-20
+    p-14 md:p-20 lg:p-36'>
+
+      <h1 className='text-center text-5xl font-light'>
+        Experience</h1>
+
+      <div className='flex flex-wrap gap-0 md:flex-nowrap md:gap-10'>
+
+        <div className='flex-1'>
+
+          <div className="relative space-y-1 border-l-2 p-8 
+          border-gray-400 text-left">
+
+            <h3 className='text-xl font-semibold md:text-2xl'>Lineage Logistics</h3>
+
+            <p className='font-light text-lg text-gray-700'>Intern, Jan 2025 - Present</p>
+
+            <p className='text-sm text-gray-700'>
+              Starting Soon!</p>
+
+            <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full
+            bg-sky-700'>
+            </span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-8 
+          border-gray-400 text-left">
+
+            <h3 className='text-xl font-semibold md:text-2xl'>Simulated Annealing Haplotype 
+              Aligner Research Project @ UCI</h3>
+
+            <p className='font-light text-lg text-gray-700'>Developer, Sep 2024 - Present </p>
+
+            <p className='text-sm text-gray-700'>
+            Enhanced and optimized haplotype assembly algorithms by refining genome read 
+            prioritization strategies, achieving a 20% reduction in runtime for datasets 
+            with over 500 single nucleotide polymorphisms (SNPs). Contributed to solving 
+            the Weighted Minimum Error Correction (wMEC) problem for diploid data by 
+            implementing advanced C++ modifications to the existing SAHap algorithm.
+            </p>
+
+            <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full
+            bg-sky-700'>
+            </span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-8 
+          border-gray-400 text-left">
+
+            <h3 className='text-xl font-semibold md:text-2xl'>Paul Merage School of Business</h3>
+
+            <p className='font-light text-lg text-gray-700'>IT Intern, July 2024 - Present</p>
+
+            <p className='text-sm text-gray-700'>
+            Enhanced operational efficiency across Windows, Mac OS, and Linux platforms by upgrading 
+            and maintaining systems, resulting in faster performance and smoother workflows. Optimized 
+            the troubleshooting and help ticket process for WiFi and networked printer issues, resulting 
+            in faster resolution times for UCI staff and students. Communicated complex technical issues 
+            to non-technical users, fostering better understanding and reducing confusion during support 
+            interactions. </p>
+
+            <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full
+            bg-sky-700'>
+            </span>
+          </div>
+        
+        </div>
+
+        <div className='flex-1'>
+
+          <div className="relative space-y-1 border-l-2 p-8 
+          border-gray-400 text-left">
+
+              <h3 className='text-xl font-semibold md:text-2xl'>UCI ICS Student Council</h3>
+
+              <p className='font-light text-lg text-gray-700'>PR Team Member, Dec 2023 - Present</p>
+
+              <p className='text-sm text-gray-700'>
+              Collaborated with committee members on all external communications such as blog posts, 
+              newsletters, social media, etc. Hosted a series of interviews called “Humans of ICS” 
+              to recognize and appreciate outstanding members of the ICS faculty. </p>
+
+              <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full
+              bg-sky-700'>
+              </span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-8 
+          border-gray-400 text-left">
+
+              <h3 className='text-xl font-semibold md:text-2xl'>PONDER Lab @ CUNY Hunter College</h3>
+
+              <p className='font-light text-lg text-gray-700'>Research Intern, July 2022 – Aug 2022 </p>
+
+              <p className='text-sm text-gray-700'>
+              Evaluated the performance of a developing Eclipse plugin that hybridizes code between 
+              eager and deferred execution to optimize performance. Discovered a bug in the program 
+              that caused errors during testing and assisted the principal investigator in fixing it.
+              Authored a research paper analyzing the results of the data and showcased my completed 
+              research at an NYU Symposium. </p>
+
+              <span className='absolute -left-[11px] top-10 h-5 w-5 rounded-full
+              bg-sky-700'>
+              </span>
+          </div>
+
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import image from '../../public/image.png'
+import pfp from '../../public/image.png'
 import { BiLogoGithub, BiLogoLinkedin } from 'react-icons/bi'
 
 const Home = () => {
     return (
-        <div id='home' className='flex min-h-screen
+        <div id='Home' className='flex min-h-screen
         w-full items-center justify-center'>
 
             <div className='flex flex-col items-center
             justify-center gap-8 p-5 text-center'>
 
-                <img src={image} alt='' className='w-[300px]
+                <img src={pfp} alt='' className='w-[300px]
                 sm:w-[350px] rounded-full'/>
 
                 <div className="space-y-1 sm:space-y-3">
@@ -42,7 +42,8 @@ const Home = () => {
                         hover:text-sky-700 md:h-12 md:w-12'/>
                     </a>
 
-                    <a href="https://www.linkedin.com/in/pranavigpalli/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/pranavigpalli/" target="_blank" 
+                    rel="noopener noreferrer">
                         <BiLogoLinkedin className='h-10 w-10 cursor-pointer rounded-full border-2
                         border-transparent bg-sky-700 p-2 text-white transition-all
                         duration-200 hover:scale-110 hover-sky-700 hover:bg-white
