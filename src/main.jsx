d5f4235daee2808b660ac15c1e0c30650e820778
Pranavi Gollanapalli <pgollana@uci.edu>
2025-01-07 19:39:03 -0800

    added router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path = "/" element={<App />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
