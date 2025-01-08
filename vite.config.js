6e18e8e737e029d2389310a872a066572d805919
Pranavi Gollanapalli <pgollana@uci.edu>
2025-01-07 20:17:43 -0800

    downloaded gh pages vite plugin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pranavigpalli.github.io/",
   plugins: [react()],
   build: {
     outDir: 'dist',
