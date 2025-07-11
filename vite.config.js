5f8996ead69b1e0c2bd27ec93fb23ef49cc5554e
Pranavi Gollanapalli <pgollana@uci.edu>
2025-07-11 00:21:13 -0700

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",
+  base: '/pranavigpalli.github.io/', // <-- THIS IS ESSENTIAL
   plugins: [react()],
   build: {
     outDir: 'dist',
